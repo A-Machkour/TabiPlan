cebfb159f3c3bc351576c2c506439f81b2833746
Machkour Abdoul <machkour.abdoul@web-atrio.com>
2023-06-15 10:18:17 +0200

    remove output.css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex flex-row ">
+    <div className="flex flex-row">
       <h1 className="flex flex-row font-bold">Header</h1>
       <ul className="flex">
         <li>Home</li>
